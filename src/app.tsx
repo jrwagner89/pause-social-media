5b1f3fb45cf52807da5dcae452fb8ac89c867dd0
jrwagner89 <jrwagner89@gmail.com>
2021-12-02 18:24:31 -0800

    Countdown clock

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './app.scss';
 
+
 export const App: React.FC = () => {
     const [isPageVisible, setIsPageVisible] = React.useState<boolean>(true);
     return (
